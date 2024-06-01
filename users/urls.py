554ad1c34fbda9ccbf8bf19a54021959e2bd88af
JustinACoder <justin.tovich@gmail.com>
2024-06-01 11:31:11 -0400

    Added profile back

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
+from django.shortcuts import redirect
 from django.urls import path
 from django.contrib.auth.views import LoginView, PasswordResetView
 import users.views
 
 urlpatterns = [
-    path('profile/', users.views.account_settings, name='account_settings'),
+    path('settings/', users.views.account_settings, name='account_settings'),
+    path('profile/', lambda request: redirect('account_profile', request.user.username), name='account_profile'),
+    path('profile/<str:username>/', users.views.account_profile, name='account_profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from discussion.models import Discussion
 from users.forms import ViewEditUserForm
+from allauth.account.views import EmailView
 
 
 @login_required
@@ -11,11 +12,22 @@ def account_settings(request):
     # Get the user object
     user = request.user  # type: User
 
+    return render(request, 'user/settings.html')
+
+
+def account_profile(request, username):
+    # If the user comes from /profile/ without a username, it means they are not logged in
+    if username == '':
+        return redirect('account_login')
+
+    # Get the user object for the profile
+    profile_user = get_object_or_404(User, username=username)
+
     # Get the list of pending discussion requests
-    pending_requests = user.discussionrequest_set.select_related('debate').all()
+    pending_requests = profile_user.discussionrequest_set.select_related('debate').all()
 
     # Get the list of stances taken
-    stances = user.stance_set.select_related('debate').all()
+    stances = profile_user.stance_set.select_related('debate').all()
 
     # Get some stats
     # TODO: transform into a single query
@@ -27,16 +39,17 @@ def account_settings(request):
     stats = {
         'stance_count': stances.count(),
         'pending_request_count': pending_requests.count(),
-        'discussion_count': Discussion.objects.filter(Q(participant1=user) | Q(participant2=user)).count(),
-        'message_count': user.message_set.count(),
-        'comment_count': user.comment_set.count(),
+        'discussion_count': Discussion.objects.filter(
+            Q(participant1=profile_user) | Q(participant2=profile_user)).count(),
+        'message_count': profile_user.message_set.count(),
+        'comment_count': profile_user.comment_set.count(),
     }
 
     context = {
-        'user_form': ViewEditUserForm(instance=user),
+        'profile_user': profile_user,
         'pending_requests': pending_requests,
         'stances': stances,
         'stats': stats,
     }
 
-    return render(request, 'user/settings.html', context)
+    return render(request, 'user/profile.html', context)
