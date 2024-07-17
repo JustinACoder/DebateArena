739d9af39d8dac4876430a831fab3f38411076fd
JustinACoder <justin.tovich@gmail.com>
2024-07-17 02:57:50 -0400

    Redesigned debate page (still need testing)

--- debate/urls.py
+++ debate/urls.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 from django.urls import include, path, reverse
 import debate.views
 from debateme.urls import debate_urlpatterns as debateme_debate_urlpatterns
@@ -9,11 +10,12 @@ main_urlpatterns = [
 ]
 
 urlpatterns = [
-    path('', lambda request: HttpResponseRedirect(reverse('debate_explore'))),
+    path('', lambda request: redirect('debate_explore')),
     path('<slug:debate_slug>/', include([
         path('', debate.views.debate, name='debate'),
         path('stance/', debate.views.set_stance, name='set_stance'),
         path('request_discussion/', debate.views.request_discussion, name='request_discussion'),
         path('vote/', debate.views.vote, name='vote'),
+        path('comment/', debate.views.comment, name='comment'),
     ] + debateme_debate_urlpatterns)),  # TODO: figure out a better way to manage these URLs...
 ]
