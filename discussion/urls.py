97bb7c733871b050e14e76469c6ad7fbfc28dd94
JustinACoder <justin.tovich@gmail.com>
2024-08-22 20:29:05 -0400

    Added paginator to discussion side panel + corrected timeago bug

--- discussion/urls.py
+++ discussion/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', discussion.views.discussion_default, name='discussion_default'),
     path('<int:discussion_id>/', discussion.views.specific_discussion, name='specific_discussion'),
     path('<int:discussion_id>/messages/', discussion.views.retrieve_messages, name='retrieve_messages'),
+    path('page/', discussion.views.get_discussion_page, name='get_discussion_page'),
 ]
--- discussion/views.py
+++ discussion/views.py
@@ -1,8 +1,11 @@
+import time
+
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.db.models import Q, Window, F, BooleanField, When, Case, Value, TextField, OuterRef, Subquery
+from django.core.paginator import Paginator
+from django.db.models import Q, Window, F, BooleanField, When, Case, Value, TextField, OuterRef, Subquery, Max
 from django.contrib.auth.decorators import login_required
-from django.db.models.functions import FirstValue, Coalesce
+from django.db.models.functions import FirstValue, Coalesce, Greatest
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db import connection
 from django.views.decorators.http import require_POST, require_GET
@@ -24,102 +27,61 @@ def dictfetchall(cursor):
     return [dict(zip(columns, row)) for row in cursor.fetchall()]
 
 
+def get_most_recent_discussions_queryset(user):
+    """
+    Get a queryset with the most recent discussions for the user. A discussion datetime is the maximum datetime
+    between the discussion creation datetime and the most recent message datetime if the discussion has messages.
+
+    :param user: User instance
+    :return: Queryset of discussions with the most recent datetime
+    """
+    # TODO: denormalize the db by adding a latest_message one-to-one field to Discussion for large scale
+    return Discussion.objects.annotate(
+        latest_message_date=Max('message__created_at'),  # Using reverse relation from Discussion to Message
+        latest_message_text=Max('message__text'),
+        latest_message_author=Max('message__author__username'),
+    ).annotate(
+        recent_date=Greatest(
+            'created_at',
+            F('latest_message_date')  # Handling null cases directly with Greatest
+        )
+    ).filter(
+        Q(participant1=user) | Q(participant2=user)
+    ).select_related('debate').order_by('-recent_date')
+
+
 @login_required
 def discussion_default(request):
-    # Get the most recent discussion for the current user
-    # most_recent_discussion_id = (Message.objects.filter(
-    #     Q(discussion__participant1=request.user) | Q(discussion__participant2=request.user)
-    # ).order_by('-created_at').values_list('discussion_id', flat=True).first())
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """
-            SELECT dd.id
-            FROM discussion_discussion dd
-                     LEFT JOIN discussion_message dm ON dd.id = dm.discussion_id
-            WHERE dd.participant1_id = %s OR dd.participant2_id = %s
-            ORDER BY COALESCE(dm.created_at, dd.created_at) DESC
-            LIMIT 1;
-            """,
-            [request.user.id, request.user.id]
-        )
-        result = cursor.fetchone()
-        most_recent_discussion_id = result[0] if result else None
-
+    # Get the most recent discussion
+    most_recent_discussion = get_most_recent_discussions_queryset(request.user).first()
+    most_recent_discussion_id = most_recent_discussion.id if most_recent_discussion else None
     return specific_discussion(request, most_recent_discussion_id)
 
 
 @login_required
 def specific_discussion(request, discussion_id):
-    # The problem with the orm query is that I can't find a clear AND efficient way of getting both
-    # the conversations with and without messages with the rest of the information.
-    # The commented out orm query below is the closest I got. It retrieves all discussions with the most recent
-    # message, but it doesn't retrieve discussions without messages.
-    # I tried using union, but it doesn't work well since it changes the order of the fields.
-    # discussions_info = (
-    #     Message.objects.filter(
-    #         Q(discussion__participant1=request.user) | Q(discussion__participant2=request.user)
-    #     ).annotate(
-    #         latest_message=Window(
-    #             expression=FirstValue('id'),
-    #             partition_by=[F('discussion_id')],
-    #             order_by=['-created_at']
-    #         )
-    #     ).filter(
-    #         latest_message=F('id')
-    #     ).values(
-    #         'discussion_id', 'discussion__debate__title', 'discussion__participant1__username',
-    #         'discussion__participant2__username', 'text', 'created_at'
-    #     ).order_by('-created_at')
-    # )
+    # If the current user is not a participant in the discussion, return 403
+    if discussion_id is not None and not Discussion.objects.filter(Q(participant1=request.user) | Q(participant2=request.user), pk=discussion_id).exists():
+        return HttpResponseForbidden()
 
-    # Get discussions with the most recent message or without messages
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """
-            SELECT *
-            FROM (SELECT dd.id                                                                               as discussion_id,
-                         dd.debate_id                                                                        as debate_id,
-                         db.title                                                                            as debate_title,
-                         dd.created_at                                                                       as discussion_created_at,
-                         dm.id                                                                               as message_id,
-                         dm.text                                                                             as message_text,
-                         dm.created_at                                                                       as message_created_at,
-                         p1.username                                                                         as participant1_username,
-                         p2.username                                                                         as participant2_username,
-                         p1.id                                                                               as participant1_id,
-                         p2.id                                                                               as participant2_id,
-                         FIRST_VALUE(dm.id) OVER (PARTITION BY dm.discussion_id ORDER BY dm.created_at DESC) as first_message_id,
-                         COALESCE(dm.created_at, dd.created_at)                                              as latest_activity_at
-                  FROM discussion_discussion dd
-                           JOIN auth_user p1 ON dd.participant1_id = p1.id
-                           JOIN auth_user p2 ON dd.participant2_id = p2.id
-                           LEFT JOIN discussion_message dm ON dd.id = dm.discussion_id
-                           JOIN debate_debate db ON dd.debate_id = db.id
-                  WHERE (dd.participant1_id = %s OR dd.participant2_id = %s)) as sub
-            WHERE message_id = first_message_id
-               OR first_message_id IS NULL
-            ORDER BY latest_activity_at DESC;
-            """,
-            [request.user.id, request.user.id]
-        )
-        discussions_info = dictfetchall(cursor)
-
-    # If this view is called with a discussion_id but the query is empty, it means that the discussion doesn't exist
-    # OR the user is not a participant. Therefore, we will redirect to the default discussion view.
-    if not discussions_info and discussion_id:
-        return redirect('discussion_default')  # TODO: should we return an error instead?
-
-    # Get Message form
-    message_form = MessageForm()
-
-    # Create context
     context = {
-        'discussions_info': discussions_info,
-        'message_form': message_form,
+        'message_form': MessageForm(),
         'discussion_id': discussion_id
     }
 
     return render(request, 'discussion/discussion_board.html', context=context)
+
+
+@login_required
+def get_discussion_page(request):
+    discussions_queryset = get_most_recent_discussions_queryset(request.user)
+
+    # Get the page
+    paginator = Paginator(discussions_queryset, 15)
+    page = paginator.get_page(request.GET.get('page', '1'))
+
+    return render(request, 'discussion/discussion_list_page.html', {'page': page})
+
 
 
 @login_required
