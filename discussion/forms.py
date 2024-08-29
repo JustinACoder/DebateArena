61d5430921e07b156573886c946f3f417e42f5e2
JustinACoder <justin.tovich@gmail.com>
2024-08-29 12:00:47 -0400

    Made message box auto growing in height

--- discussion/forms.py
+++ discussion/forms.py
@@ -3,12 +3,7 @@ from .models import Message
 
 
 class MessageForm(forms.ModelForm):
-    text = forms.CharField(widget=forms.Textarea(attrs={
-        'rows': 1,
-        'id': 'message-text-input',
-        'style': 'resize: none; overflow: hidden;',
-        'placeholder': 'Type your message here...'
-    }), label='', required=True, max_length=5000, strip=True)
+    text = forms.CharField(required=True, max_length=5000, strip=True)
 
     class Meta:
         model = Message
